029936fd9fa3783997d2be796d3cc16cb342666f
mohamed <memoelgammal@outlook.com>
2022-10-30 09:15:38 +0200

    try this

--- Module5-Solution/js/script.js
+++ Module5-Solution/js/script.js
@@ -83,10 +83,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   homeHtmlUrl,
-  function(responseText){
-    document.querySelector("#main-content")
-    .innerHTML = responseText;
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
+  //function(responseText){
+    //document.querySelector("#main-content")
+    //.innerHTML = responseText;
+  //}, // ***** <---- TODO: STEP 1: Substitute [...] ******
   false); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
